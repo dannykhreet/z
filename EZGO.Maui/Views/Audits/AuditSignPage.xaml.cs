4c7eaf3d9b8a8eb5327e561568a9fa8964bd65c7
Danny khreet <42088706+dannykhreet@users.noreply.github.com>
2025-08-28 15:10:53 +0200

    Close popup and unsubscribe events on disappear

--- EZGO.Maui/Views/Audits/AuditSignPage.xaml.cs
+++ EZGO.Maui/Views/Audits/AuditSignPage.xaml.cs
@@ -12,4 +12,16 @@ public partial class AuditSignPage : ContentPage
         PopupLayout.ContentTemplate = new DataTemplate();
         PopupLayout.ContentTemplate = AutocompleteTemplate;
     }
+
+    protected override void OnDisappearing()
+    {
+        if (PopupLayout.IsOpen)
+        {
+            PopupLayout.IsOpen = false;
+        }
+
+        PopupLayout.Opened -= PopupLayout_Opened;
+
+        base.OnDisappearing();
+    }
 }
